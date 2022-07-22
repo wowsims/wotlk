ed1d33ce89b6970a637ef989bebfd2bd31034303
dbyena <dkeyes@DESKTOP-7JOF975.localdomain>
2022-07-21 19:52:10 -0700

    Preset issues

--- ui/raid/presets.ts
+++ ui/raid/presets.ts
@@ -444,10 +444,10 @@ export const playerPresets: Array<PresetSpecSettings<any>> = [
 		defaultGear: {
 			[Faction.Unknown]: {},
 			[Faction.Alliance]: {
-				1: ShadowPriestPresets.P1_PRESET.gear,
+				1: ShadowPriestPresets.P5_PRESET.gear,
 			},
 			[Faction.Horde]: {
-				1: ShadowPriestPresets.P1_PRESET.gear,
+				1: ShadowPriestPresets.P5_PRESET.gear,
 			},
 		},
 		tooltip: specNames[Spec.SpecShadowPriest],
