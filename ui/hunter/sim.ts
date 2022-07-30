684c230545ef7f3b96823a8ea340614d0e99cc54
James Tanner <jimmyt857@gmail.com>
2022-07-30 10:45:58 -0700

    Fix hunter pet scaling

--- ui/hunter/sim.ts
+++ ui/hunter/sim.ts
@@ -132,6 +132,7 @@ export class HunterSimUI extends IndividualSimUI<Spec.SpecHunter> {
 				// Default raid/party buffs settings.
 				raidBuffs: RaidBuffs.create({
 					arcaneBrilliance: true,
+					powerWordFortitude: TristateEffect.TristateEffectImproved,
 					giftOfTheWild: TristateEffect.TristateEffectImproved,
 					bloodlust: true,
 					strengthOfEarthTotem: TristateEffect.TristateEffectImproved,
@@ -148,6 +149,7 @@ export class HunterSimUI extends IndividualSimUI<Spec.SpecHunter> {
 					blessingOfKings: true,
 					blessingOfWisdom: 2,
 					blessingOfMight: 2,
+					vampiricTouch: true,
 				}),
 				debuffs: Debuffs.create({
 					sunderArmor: true,
@@ -171,6 +173,7 @@ export class HunterSimUI extends IndividualSimUI<Spec.SpecHunter> {
 			],
 			// Buff and Debuff inputs to include/exclude, overriding the EP-based defaults.
 			includeBuffDebuffInputs: [
+				IconInputs.StaminaBuff,
 				IconInputs.SpellDamageDebuff,
 			],
 			excludeBuffDebuffInputs: [
