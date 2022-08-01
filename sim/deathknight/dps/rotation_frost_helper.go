1e3cf845956e76d0983af835b7fdc171c732d57c
JooperGH <alexandredmfs@gmail.com>
2022-08-01 21:38:19 +0100

    Removed some old code

--- sim/deathknight/dps/rotation_frost_helper.go
+++ sim/deathknight/dps/rotation_frost_helper.go
@@ -1,8 +1,6 @@
 package dps
 
 import (
-	"time"
-
 	"github.com/wowsims/wotlk/sim/core"
 )
 
@@ -27,60 +25,4 @@ func (dk *DpsDeathknight) FrostRotationCast(sim *core.Simulation, target *core.U
 		fr.lastSpell = spell
 	}
 	return canCast
-}
-
-func (dk *DpsDeathknight) FrostDiseaseCheck(sim *core.Simulation, target *core.Unit, spell *core.Spell, costRunes bool, casts int) bool {
-	ffRemaining := dk.FrostFeverDisease[target.Index].RemainingDuration(sim)
-	bpRemaining := dk.BloodPlagueDisease[target.Index].RemainingDuration(sim)
-	castGcd := dk.SpellGCD() * time.Duration(casts)
-
-	if !dk.FrostFeverDisease[target.Index].IsActive() || ffRemaining < castGcd {
-		// Refresh FF
-		return false
-	}
-	if !dk.BloodPlagueDisease[target.Index].IsActive() || bpRemaining < castGcd {
-		// Refresh BP
-		return false
-	}
-
-	if dk.CanCast(sim, spell) && costRunes {
-		ffExpiresAt := ffRemaining + sim.CurrentTime
-		bpExpiresAt := bpRemaining + sim.CurrentTime
-
-		crpb := dk.CopyRunicPowerBar()
-		runeCostForSpell := dk.RuneAmountForSpell(spell)
-		spellCost := crpb.DetermineOptimalCost(sim, runeCostForSpell.Blood, runeCostForSpell.Frost, runeCostForSpell.Unholy)
-
-		crpb.Spend(sim, spell, spellCost)
-
-		afterCastTime := sim.CurrentTime + castGcd
-		currentBloodRunes := crpb.CurrentBloodRunes()
-		nextBloodRuneAt := crpb.BloodRuneReadyAt(sim)
-
-		// Check FF
-		if dk.frCheckForDiseaseRecast(ffExpiresAt, afterCastTime, spellCost.Blood, currentBloodRunes, nextBloodRuneAt) {
-			return false
-		}
-
-		// Check BP
-		if dk.frCheckForDiseaseRecast(bpExpiresAt, afterCastTime, spellCost.Blood, currentBloodRunes, nextBloodRuneAt) {
-			return false
-		}
-	} else {
-		return false
-	}
-
-	return true
-}
-
-func (dk *DpsDeathknight) frCheckForDiseaseRecast(expiresAt time.Duration, afterCastTime time.Duration,
-	spellCost int, currentRunes int32, nextRuneAt time.Duration) bool {
-	if spellCost > 0 && currentRunes == 0 {
-		if expiresAt < nextRuneAt {
-			return true
-		}
-	} else if afterCastTime > expiresAt {
-		return true
-	}
-	return false
 }
