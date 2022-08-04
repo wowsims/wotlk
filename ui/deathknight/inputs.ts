20a86a5f19d6f6c0e7a5b5ba6c1ff1bf8c44250f
rosenrusinov <rainisunbelievable@gmail.com>
2022-08-04 18:39:14 +0200

    fix dk input change emitters

--- ui/deathknight/inputs.ts
+++ ui/deathknight/inputs.ts
@@ -58,7 +58,7 @@ export const UseDeathAndDecay = InputHelpers.makeRotationBooleanInput<Spec.SpecD
 	label: 'Death and Decay',
 	labelTooltip: 'Use Death and Decay based rotation.',
 	showWhen: (player: Player<Spec.SpecDeathknight>) => player.getTalents().summonGargoyle && player.getTalents().scourgeStrike,
-	changeEmitter: (player: Player<Spec.SpecDeathknight>) => player.talentsChangeEmitter,
+	changeEmitter: (player: Player<Spec.SpecDeathknight>) => TypedEvent.onAny([player.rotationChangeEmitter, player.talentsChangeEmitter]),
 });
 
 export const SetDeathAndDecayPrio = InputHelpers.makeRotationEnumInput<Spec.SpecDeathknight, DeathAndDecayPrio>({
@@ -86,7 +86,7 @@ export const BloodTapGhoulFrenzy = InputHelpers.makeRotationBooleanInput<Spec.Sp
 	label: 'BT Ghoul Frenzy',
 	labelTooltip: 'Use Ghoul Frenzy only with Blood Tap.',
 	showWhen: (player: Player<Spec.SpecDeathknight>) => player.getTalents().ghoulFrenzy,
-	changeEmitter: (player: Player<Spec.SpecDeathknight>) => player.talentsChangeEmitter,
+	changeEmitter: (player: Player<Spec.SpecDeathknight>) => TypedEvent.onAny([player.rotationChangeEmitter, player.talentsChangeEmitter]),
 });
 
 export const SetFirstDisease = InputHelpers.makeRotationEnumInput<Spec.SpecDeathknight, FirstDisease>({
@@ -98,7 +98,7 @@ export const SetFirstDisease = InputHelpers.makeRotationEnumInput<Spec.SpecDeath
 		{ name: 'Blood Plague', value: FirstDisease.BloodPlague },
 	],
 	showWhen: (player: Player<Spec.SpecDeathknight>) => player.getTalents().summonGargoyle,
-	changeEmitter: (player: Player<Spec.SpecDeathknight>) => player.talentsChangeEmitter,
+	changeEmitter: (player: Player<Spec.SpecDeathknight>) => TypedEvent.onAny([player.rotationChangeEmitter, player.talentsChangeEmitter]),
 })
 
 export const UseArmyOfTheDead = InputHelpers.makeRotationEnumInput<Spec.SpecDeathknight, ArmyOfTheDead>({
