2e99be8e7659cdd25b475b67cb5589315dfcb53a
James Tanner <jimmyt857@gmail.com>
2022-08-03 18:03:34 -0700

    Update enchant descriptions

---
+++ ui/core/proto_utils/enchants.ts
@@ -0,0 +1,22 @@
+import { Enchant } from '../proto/common.js';
+
+let descriptionsPromise: Promise<Record<number, string>> | null = null;
+function fetchEnchantDescriptions(): Promise<Record<number, string>> {
+	if (descriptionsPromise == null) {
+		descriptionsPromise = fetch('/wotlk/assets/enchants/descriptions.json')
+		.then(response => response.json())
+		.then(json => {
+			const descriptionsMap: Record<number, string> = {};
+			for (let idStr in json) {
+				descriptionsMap[parseInt(idStr)] = json[idStr];
+			}
+			return descriptionsMap;
+		});
+	}
+	return descriptionsPromise;
+}
+
+export async function getEnchantDescription(enchant: Enchant): Promise<string> {
+	const descriptionsMap = await fetchEnchantDescriptions();
+	return descriptionsMap[enchant.id] || enchant.name;
+}
