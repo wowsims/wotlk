dfc9bc72c29dedc664ba70fda66ef872cbb291b8
JooperGH <alexandredmfs@gmail.com>
2022-08-06 19:19:13 +0100

    Improved sub blood rotation

--- sim/deathknight/dps/rotation_frost.go
+++ sim/deathknight/dps/rotation_frost.go
@@ -5,7 +5,11 @@ import (
 )
 
 type FrostRotation struct {
+	oblitCount  int32
+	missedPesti bool
 }
 
 func (fr *FrostRotation) Reset(sim *core.Simulation) {
+	fr.oblitCount = 0
+	fr.missedPesti = false
 }
