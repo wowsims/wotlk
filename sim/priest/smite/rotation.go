3a4a9217ea868f243975882ea501f1c6ca53c7dc
James Tanner <jimmyt857@gmail.com>
2022-08-22 21:38:06 -0700

    Implement penance

--- sim/priest/smite/rotation.go
+++ sim/priest/smite/rotation.go
@@ -36,6 +36,8 @@ func (spriest *SmitePriest) chooseSpell(sim *core.Simulation) *core.Spell {
 		return spriest.ShadowWordPain
 	} else if spriest.rotation.UseDevouringPlague && !spriest.DevouringPlagueDot.IsActive() {
 		return spriest.DevouringPlague
+	} else if spriest.Penance != nil && spriest.Penance.IsReady(sim) {
+		return spriest.Penance
 	} else if spriest.rotation.UseShadowWordDeath && spriest.ShadowWordDeath.IsReady(sim) {
 		return spriest.ShadowWordDeath
 	} else if spriest.rotation.UseMindBlast && spriest.MindBlast.IsReady(sim) {
