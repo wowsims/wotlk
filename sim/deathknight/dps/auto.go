d15dcd6187d749e986ad782a275b6dd9c6f48f06
Ben Echols <lologarithm@gmail.com>
2022-08-29 11:05:55 -0600

    more small changes

--- sim/deathknight/dps/auto.go
+++ sim/deathknight/dps/auto.go
@@ -68,17 +68,11 @@ func (dk *DpsDeathknight) RotationActionCallback_AutoDW(sim *core.Simulation, ta
 		return dk.NextGCDAt()
 	}
 
-	// 1. If you have desolation - Maintain Desolation
-	// 2. Maintain Frost Fever
-	// 3. Maintain Blood Plague
-	// 4. Maintain DnD
-	// 5. Use Gary
-	// 7. Use Runic Power on Death Coil
-
-	// If we need to use DnD and we don't have the runes, pop ERW
+	// If we don't have any runes, might as well pop ERW.
 	if dk.EmpowerRuneWeapon.IsReady(sim) && dk.AllRunesSpent() {
 		dk.EmpowerRuneWeapon.Cast(sim, dk.CurrentTarget)
 		if dk.Presence == deathknight.UnholyPresence {
+			// good time to swap stances too
 			dk.BloodPresence.Cast(sim, dk.CurrentTarget)
 		}
 	}
@@ -87,7 +81,7 @@ func (dk *DpsDeathknight) RotationActionCallback_AutoDW(sim *core.Simulation, ta
 	// If we need GF and we can't cast it right now, but BT is ready, lets use it!
 	canGFWithBT := !dk.GhoulFrenzyAura.IsActive() && dk.BloodTap.CanCast(sim)
 
-	if dk.Talents.Desolation > 0 && !dk.DesolationAura.IsActive() && dk.BloodStrike.CanCast(sim) {
+	if dk.Talents.Desolation > 0 && dk.DesolationAura.RemainingDuration(sim) < time.Second && dk.BloodStrike.CanCast(sim) {
 		dk.BloodStrike.Cast(sim, dk.CurrentTarget)
 	} else if dk.FrostFeverDisease[dk.CurrentTarget.Index].RemainingDuration(sim) < time.Second*4 && dk.IcyTouch.CanCast(sim) {
 		dk.IcyTouch.Cast(sim, dk.CurrentTarget)
@@ -104,8 +98,11 @@ func (dk *DpsDeathknight) RotationActionCallback_AutoDW(sim *core.Simulation, ta
 		dk.SummonGargoyle.Cast(sim, dk.CurrentTarget)
 	} else {
 		dndAt := dk.DeathAndDecay.ReadyAt()
-		if dndAt > sim.CurrentTime {
 
+		// Prio DC if we have a lot of extra RP
+		if dk.DeathCoil.CanCast(sim) && dk.CurrentRunicPower() > 80 {
+			dk.DeathCoil.Cast(sim, dk.CurrentTarget)
+		} else if dndAt > sim.CurrentTime {
 			numDeath := dk.CurrentDeathRunes()
 			numBlood := dk.CurrentBloodRunes()
 			numFrost := dk.CurrentFrostRunes()
@@ -117,17 +114,21 @@ func (dk *DpsDeathknight) RotationActionCallback_AutoDW(sim *core.Simulation, ta
 
 			if numDeath+numBlood >= 2 || (numBlood == 1 && nbr < dndAt) {
 				dk.BloodBoil.Cast(sim, dk.CurrentTarget)
-			} else if numDeath+numFrost >= 2 || (numFrost == 1 && nfr < dndAt) {
-				dk.IcyTouch.Cast(sim, dk.CurrentTarget)
-			} else if numDeath+numUnholy >= 2 || (numUnholy == 1 && nur < dndAt) {
-				dk.PlagueStrike.Cast(sim, dk.CurrentTarget)
+			} else {
+				// TODO: check which one has the shorter duration for the DOT.
+				if numDeath+numFrost >= 2 || (numFrost == 1 && nfr < dndAt) {
+					dk.IcyTouch.Cast(sim, dk.CurrentTarget)
+				} else if numDeath+numUnholy >= 2 || (numUnholy == 1 && nur < dndAt) {
+					dk.PlagueStrike.Cast(sim, dk.CurrentTarget)
+				}
 			}
 		}
 
+		// If we didn't do anything else spend some RP or make some more
 		if dk.GCD.IsReady(sim) {
 			if dk.DeathCoil.CanCast(sim) {
 				dk.DeathCoil.Cast(sim, dk.CurrentTarget)
-			} else if dk.HornOfWinter.CanCast(sim) && dk.CurrentRunicPower() < 80 {
+			} else if dk.HornOfWinter.CanCast(sim) && (dk.CurrentRunicPower() < 80 || !dk.HornOfWinterAura.IsActive()) {
 				dk.HornOfWinter.Cast(sim, dk.CurrentTarget)
 			} else {
 				waitUntil := dk.RunicPowerBar.AnySpentRuneReadyAt()
