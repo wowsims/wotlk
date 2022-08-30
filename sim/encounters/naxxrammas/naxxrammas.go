437b82820c45740d6c8da579ec74fe11a66ad6c3
JooperGH <alexandredmfs@gmail.com>
2022-08-30 01:09:22 +0100

    Added temporary Kel'Thuzad and Thaddius

--- sim/encounters/naxxrammas/naxxrammas.go
+++ sim/encounters/naxxrammas/naxxrammas.go
@@ -2,6 +2,8 @@ package naxxrammas
 
 func Register() {
 	addPatchwerk25("Naxxrammas")
+	addKelThuzad25("Naxxrammas")
+	addThaddius25("Naxxrammas")
 
 	// TODO: Figure out why this isn't pickable
 	//addPatchwerk10("Naxxrammas")
