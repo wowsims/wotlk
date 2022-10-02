1f277ffdff2cfbc6b51f486dcca3bf5ad7b96ac3
Jean-Hugues Guinot <jean-hugues.guinot@cairn.info>
2022-10-02 16:56:47 +0200

    Wrath of Cenarius fix

--- sim/druid/wrath.go
+++ sim/druid/wrath.go
@@ -15,7 +15,7 @@ func (druid *Druid) registerWrathSpell() {
 	actionID := core.ActionID{SpellID: 48461}
 	baseCost := 0.11 * druid.BaseMana
 	manaMetrics := druid.NewManaMetrics(core.ActionID{SpellID: 24858})
-	spellCoeff := 0.571 * (1 + 0.02*float64(druid.Talents.WrathOfCenarius))
+	spellCoeff := 0.571 + (0.02 * float64(druid.Talents.WrathOfCenarius))
 	bonusFlatDamage := core.TernaryFloat64(druid.Equip[items.ItemSlotRanged].ID == IdolAvenger, 25, 0) +
 		core.TernaryFloat64(druid.Equip[items.ItemSlotRanged].ID == IdolSteadfastRenewal, 70, 0)
 
