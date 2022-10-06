c9d53e876844dc3bdf84c8aa17330eaf3ab82a05
Jean-Hugues Guinot <jean-hugues.guinot@cairn.info>
2022-10-06 08:52:11 +0200

    SmartCDs simplification

--- sim/druid/balance/balance.go
+++ sim/druid/balance/balance.go
@@ -6,7 +6,6 @@ import (
 	"github.com/wowsims/wotlk/sim/core/proto"
 	"github.com/wowsims/wotlk/sim/core/stats"
 	"github.com/wowsims/wotlk/sim/druid"
-	"sort"
 )
 
 func RegisterBalanceDruid() {
@@ -43,7 +42,6 @@ func NewBalanceDruid(character core.Character, options proto.Player) *BalanceDru
 
 	moonkin.ResetTalentsBonuses()
 	moonkin.RegisterTalentsBonuses()
-	moonkin.RegisterCooldownRankings()
 	moonkin.EnableResumeAfterManaWait(moonkin.tryUseGCD)
 	return moonkin
 }
@@ -59,15 +57,15 @@ type BalanceDruid struct {
 	*druid.Druid
 
 	Rotation           proto.BalanceDruid_Rotation
-	CooldownsRanking   []BalanceCooldown
 	CooldownsAvailable []*core.MajorCooldown
 
 	// CDS
-	hyperSpeedMCD *core.MajorCooldown
-	potionMCD     *core.MajorCooldown
-	onUseTrinket1 *core.MajorCooldown
-	onUseTrinket2 *core.MajorCooldown
-	potionUsed    bool
+	hyperSpeedMCD      *core.MajorCooldown
+	potionSpeedMCD     *core.MajorCooldown
+	potionWildMagicMCD *core.MajorCooldown
+	onUseTrinket1      *core.MajorCooldown
+	onUseTrinket2      *core.MajorCooldown
+	potionUsed         bool
 }
 
 func (moonkin *BalanceDruid) GetDruid() *druid.Druid {
@@ -88,68 +86,27 @@ func (moonkin *BalanceDruid) Reset(sim *core.Simulation) {
 		consumes := &moonkin.Consumes
 
 		if consumes.DefaultPotion == proto.Potions_PotionOfSpeed {
-			moonkin.getBalanceMajorCooldown(core.ActionID{ItemID: 40211})
+			moonkin.potionSpeedMCD = moonkin.getBalanceMajorCooldown(core.ActionID{ItemID: 40211})
 		}
 		if consumes.DefaultPotion == proto.Potions_PotionOfWildMagic {
-			moonkin.getBalanceMajorCooldown(core.ActionID{ItemID: 40212})
+			moonkin.potionWildMagicMCD = moonkin.getBalanceMajorCooldown(core.ActionID{ItemID: 40212})
 		}
 		if moonkin.HasProfession(proto.Profession_Engineering) {
-			moonkin.getBalanceMajorCooldown(core.ActionID{SpellID: 54758})
+			moonkin.hyperSpeedMCD = moonkin.getBalanceMajorCooldown(core.ActionID{SpellID: 54758})
 		}
-		moonkin.getBalanceMajorCooldown(core.ActionID{ItemID: moonkin.Equip[items.ItemSlotTrinket1].ID})
-		moonkin.getBalanceMajorCooldown(core.ActionID{ItemID: moonkin.Equip[items.ItemSlotTrinket2].ID})
-
-		// Sort this array depending on moonkin.CooldownRankings
-		sort.Slice(moonkin.CooldownsAvailable, func(i, j int) bool {
-			return moonkin.CooldownsAvailable[i].Priority < moonkin.CooldownsAvailable[j].Priority
-		})
+		moonkin.onUseTrinket1 = moonkin.getBalanceMajorCooldown(core.ActionID{ItemID: moonkin.Equip[items.ItemSlotTrinket1].ID})
+		moonkin.onUseTrinket2 = moonkin.getBalanceMajorCooldown(core.ActionID{ItemID: moonkin.Equip[items.ItemSlotTrinket2].ID})
 	}
 }
 
 // Takes out a Cooldown from the generic MajorCooldownManager and adds it to a custom Slice of Cooldowns
-// Sets a stat and a priority for the cooldown, depending on moonkin.CooldownsRanking
-func (moonkin *BalanceDruid) getBalanceMajorCooldown(actionID core.ActionID) {
+func (moonkin *BalanceDruid) getBalanceMajorCooldown(actionID core.ActionID) *core.MajorCooldown {
 	if moonkin.Character.HasMajorCooldown(actionID) {
 		majorCd := moonkin.Character.GetMajorCooldown(actionID)
 		majorCd.ShouldActivate = func(sim *core.Simulation, character *core.Character) bool {
 			return false
 		}
-		for _, v := range moonkin.CooldownsRanking {
-			if v.ID.SameAction(majorCd.Spell.ActionID) {
-				majorCd.Priority = v.Priority
-				majorCd.Spell.ResourceType = v.Stat
-				moonkin.CooldownsAvailable = append(moonkin.CooldownsAvailable, majorCd)
-			}
-		}
+		return majorCd
 	}
-}
-
-func (moonkin *BalanceDruid) RegisterCooldownRankings() {
-
-	moonkin.CooldownsRanking = []BalanceCooldown{
-		{
-			Name:     "Potion of Speed",
-			Stat:     stats.SpellHaste,
-			Priority: 500,
-			ID:       core.ActionID{ItemID: 40211},
-		},
-		{
-			Name:     "Hyperspeed Acceleration",
-			Stat:     stats.SpellHaste,
-			Priority: 340,
-			ID:       core.ActionID{SpellID: 54758},
-		},
-		{
-			Name:     "Potion of Wild Magic",
-			Stat:     stats.SpellCrit,
-			Priority: 200,
-			ID:       core.ActionID{ItemID: 40212},
-		},
-		{
-			Name:     "Mark of the War Prisoner",
-			Stat:     stats.SpellPower,
-			Priority: 346,
-			ID:       core.ActionID{ItemID: 37873},
-		},
-	}
+	return nil
 }
