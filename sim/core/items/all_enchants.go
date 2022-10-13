72a0578f32681a5720f40ccb949bde1b01c513f7
Ben Echols <lologarithm@gmail.com>
2022-10-13 10:43:16 -0600

    added staff spellpower enchant

--- sim/core/items/all_enchants.go
+++ sim/core/items/all_enchants.go
@@ -272,6 +272,7 @@ var Enchants = []Enchant{
 	{ID: 22556, EffectID: 2670, Name: "2H Weapon - Major Agility", Quality: proto.ItemQuality_ItemQualityUncommon, Bonus: stats.Stats{stats.Agility: 35}, ItemType: proto.ItemType_ItemTypeWeapon, EnchantType: proto.EnchantType_EnchantTypeTwoHand},
 	{ID: 33307, EffectID: 3225, Name: "Executioner", Quality: proto.ItemQuality_ItemQualityRare, Bonus: stats.Stats{}, ItemType: proto.ItemType_ItemTypeWeapon},
 	{ID: 35498, EffectID: 3273, Name: "Deathfrost", Phase: 5, Quality: proto.ItemQuality_ItemQualityRare, Bonus: stats.Stats{}, ItemType: proto.ItemType_ItemTypeWeapon},
+	{ID: 62959, EffectID: 3855, Name: "Staff - Spellpower", Quality: proto.ItemQuality_ItemQualityRare, Bonus: stats.Stats{stats.SpellPower: 69}, ItemType: proto.ItemType_ItemTypeWeapon, EnchantType: proto.EnchantType_EnchantTypeTwoHand},
 
 	// Shield
 	{ID: 22539, EffectID: 2654, Name: "Intellect", Quality: proto.ItemQuality_ItemQualityCommon, Bonus: stats.Stats{stats.Intellect: 12}, ItemType: proto.ItemType_ItemTypeWeapon, EnchantType: proto.EnchantType_EnchantTypeShield},
