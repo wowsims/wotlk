6a9741921a6425003b80d2ac7ec1b44ab4cf22e0
Jean-Hugues Guinot <jean-hugues.guinot@cairn.info>
2022-10-26 20:32:46 +0200

    Typhoon spell

---
+++ sim/druid/typhoon.go
@@ -0,0 +1,55 @@
+package druid
+
+import (
+	"github.com/wowsims/wotlk/sim/core"
+	"github.com/wowsims/wotlk/sim/core/proto"
+	"github.com/wowsims/wotlk/sim/core/stats"
+	"time"
+)
+
+func (druid *Druid) registerTyphoonSpell() {
+	if !druid.Talents.Typhoon {
+		return
+	}
+
+	actionID := core.ActionID{SpellID: 61384}
+	baseCost := 0.25 * druid.BaseMana
+	spellCoeff := 0.193
+
+	druid.Typhoon = druid.RegisterSpell(core.SpellConfig{
+		ActionID:     actionID,
+		SpellSchool:  core.SpellSchoolNature,
+		ProcMask:     core.ProcMaskSpellDamage,
+		ResourceType: stats.Mana,
+		BaseCost:     baseCost,
+
+		Cast: core.CastConfig{
+			DefaultCast: core.Cast{
+				Cost: baseCost,
+				GCD:  core.GCDDefault,
+			},
+			CD: core.Cooldown{
+				Timer:    druid.NewTimer(),
+				Duration: time.Second * (20 - core.TernaryDuration(druid.HasMajorGlyph(proto.DruidMajorGlyph_GlyphOfMonsoon), 3, 0)),
+			},
+			ModifyCast: func(sim *core.Simulation, spell *core.Spell, cast *core.Cast) {
+				druid.applyNaturesSwiftness(cast)
+				druid.ApplyClearcasting(sim, spell, cast)
+			},
+		},
+
+		DamageMultiplier: 1 + druid.talentBonuses.galeWinds,
+		ThreatMultiplier: 1,
+		CritMultiplier:   1,
+
+		ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+			spell.WaitTravelTime(sim, func(sim *core.Simulation) {
+				baseDamage := 1190 + (spellCoeff * spell.SpellPower())
+				baseDamage *= sim.Encounter.AOECapMultiplier()
+				for _, aoeTarget := range sim.Encounter.Targets {
+					spell.CalcAndDealDamageMagicHitAndCrit(sim, &aoeTarget.Unit, baseDamage)
+				}
+			})
+		},
+	})
+}
