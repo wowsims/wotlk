f661fee7b9f93f70f0dd5be040b5c2c1b401dd53
James Tanner <jimmyt857@gmail.com>
2022-11-20 16:27:51 -0800

    Remove usage of ItemData struct

--- tools/generate_items/database.go
+++ tools/generate_items/database.go
@@ -10,60 +10,14 @@ import (
 	googleProto "google.golang.org/protobuf/proto"
 )
 
-// For overriding item data.
-type ItemOverride struct {
-	ID int
-
-	Stats          Stats // Only non-zero values will override
-	ClassAllowlist []proto.Class
-	Phase          int
-	HandType       proto.HandType // Overrides hand type.
-}
-
-type ItemData struct {
-	Response ItemResponse
-	Override ItemOverride
-}
-
-func (itemData *ItemData) toProto() *proto.UIItem {
-	weaponDamageMin, weaponDamageMax := itemData.Response.GetWeaponDamage()
-
-	itemProto := &proto.UIItem{
-		Name: itemData.Response.GetName(),
-		Icon: itemData.Response.GetIcon(),
-
-		Type:             itemData.Response.GetItemType(),
-		ArmorType:        itemData.Response.GetArmorType(),
-		WeaponType:       itemData.Response.GetWeaponType(),
-		HandType:         itemData.Response.GetHandType(),
-		RangedWeaponType: itemData.Response.GetRangedWeaponType(),
-
-		Stats:       toSlice(mergeStats(itemData.Response.GetStats(), itemData.Override.Stats)),
-		GemSockets:  itemData.Response.GetGemSockets(),
-		SocketBonus: toSlice(itemData.Response.GetSocketBonus()),
-
-		WeaponDamageMin: weaponDamageMin,
-		WeaponDamageMax: weaponDamageMax,
-		WeaponSpeed:     itemData.Response.GetWeaponSpeed(),
-
-		Ilvl:    int32(itemData.Response.GetItemLevel()),
-		Phase:   int32(itemData.Response.GetPhase()),
-		Quality: proto.ItemQuality(itemData.Response.GetQuality()),
-		Unique:  itemData.Response.GetUnique(),
-		Heroic:  itemData.Response.IsHeroic(),
-
-		ClassAllowlist:     itemData.Response.GetClassAllowlist(),
-		RequiredProfession: itemData.Response.GetRequiredProfession(),
-		SetName:            itemData.Response.GetItemSetName(),
+func mergeItemProtos(dst, src *proto.UIItem) {
+	// googleproto.Merge concatenates lists but we want replacement, so do them manually.
+	if src.Stats != nil {
+		dst.Stats = src.Stats
+		src.Stats = nil
 	}
-
-	overrideProto := &proto.UIItem{
-		Id:    int32(itemData.Override.ID),
-		Phase: int32(itemData.Override.Phase),
-	}
-
-	googleProto.Merge(itemProto, overrideProto)
-	return itemProto
+	// TODO: Other stat fields
+	googleProto.Merge(dst, src)
 }
 
 func mergeGemProtos(dst, src *proto.UIGem) {
@@ -76,7 +30,7 @@ func mergeGemProtos(dst, src *proto.UIGem) {
 }
 
 type WowDatabase struct {
-	items    []ItemData
+	items    map[int32]*proto.UIItem
 	enchants []*proto.UIEnchant
 	gems     map[int32]*proto.UIGem
 
@@ -86,22 +40,25 @@ type WowDatabase struct {
 	encounters []*proto.PresetEncounter
 }
 
-func NewWowDatabase(itemOverrides []ItemOverride, gemOverrides []*proto.UIGem, enchantOverrides []*proto.UIEnchant, itemTooltipsDB map[int]WowheadItemResponse, spellTooltipsDB map[int]WowheadItemResponse) *WowDatabase {
+func NewWowDatabase(itemOverrides []*proto.UIItem, gemOverrides []*proto.UIGem, enchantOverrides []*proto.UIEnchant, itemTooltipsDB map[int]WowheadItemResponse, spellTooltipsDB map[int]WowheadItemResponse) *WowDatabase {
 	db := &WowDatabase{
+		items:      make(map[int32]*proto.UIItem),
 		enchants:   enchantOverrides,
 		gems:       make(map[int32]*proto.UIGem),
 		encounters: core.PresetEncounters,
 	}
 
 	for _, itemOverride := range itemOverrides {
-		itemData := ItemData{
-			Override: itemOverride,
-			Response: itemTooltipsDB[itemOverride.ID],
-		}
-		if itemData.Response.GetName() == "" {
+		response := itemTooltipsDB[int(itemOverride.Id)]
+
+		if !response.IsEquippable() {
 			continue
 		}
-		db.items = append(db.items, itemData)
+
+		itemProto := response.ToItemProto()
+		itemProto.Id = itemOverride.Id
+		mergeItemProtos(itemProto, itemOverride)
+		db.items[itemProto.Id] = itemProto
 	}
 
 	for id, response := range itemTooltipsDB {
@@ -132,7 +89,7 @@ func NewWowDatabase(itemOverrides []ItemOverride, gemOverrides []*proto.UIGem, e
 
 	for _, itemID := range extraItemIcons {
 		if itemID != 0 {
-			if tooltip, ok := itemTooltipsDB[itemID]; ok {
+			if tooltip, ok := itemTooltipsDB[int(itemID)]; ok {
 				db.itemIcons = append(db.itemIcons, &proto.IconData{Id: int32(itemID), Name: tooltip.GetName(), Icon: tooltip.GetIcon()})
 			}
 		}
@@ -145,9 +102,6 @@ func NewWowDatabase(itemOverrides []ItemOverride, gemOverrides []*proto.UIGem, e
 		return icon.Name != "" && icon.Icon != ""
 	})
 
-	slices.SortStableFunc(db.items, func(i1, i2 ItemData) bool {
-		return i1.Override.ID < i2.Override.ID
-	})
 	slices.SortStableFunc(db.itemIcons, func(s1, s2 *proto.IconData) bool {
 		return s1.Id < s2.Id
 	})
@@ -162,13 +116,13 @@ func NewWowDatabase(itemOverrides []ItemOverride, gemOverrides []*proto.UIGem, e
 
 // Filters out entities which shouldn't be included anywhere.
 func (db *WowDatabase) applyGlobalFilters() {
-	db.items = core.FilterSlice(db.items, func(itemData ItemData) bool {
-		if _, ok := itemDenyList[itemData.Override.ID]; ok {
+	db.items = core.FilterMap(db.items, func(_ int32, item *proto.UIItem) bool {
+		if _, ok := itemDenyList[item.Id]; ok {
 			return false
 		}
 
 		for _, pattern := range denyListNameRegexes {
-			if pattern.MatchString(itemData.Response.GetName()) {
+			if pattern.MatchString(item.Name) {
 				return false
 			}
 		}
@@ -176,7 +130,7 @@ func (db *WowDatabase) applyGlobalFilters() {
 	})
 
 	db.gems = core.FilterMap(db.gems, func(_ int32, gem *proto.UIGem) bool {
-		if _, ok := gemDenyList[int(gem.Id)]; ok {
+		if _, ok := gemDenyList[gem.Id]; ok {
 			return false
 		}
 
@@ -190,45 +144,35 @@ func (db *WowDatabase) applyGlobalFilters() {
 }
 
 // Returns only items which are worth including in the sim.
-func (db *WowDatabase) getSimmableItems() []ItemData {
-	var included []ItemData
-	for _, itemData := range db.items {
-		if !itemData.Response.IsEquippable() {
-			continue
+func (db *WowDatabase) getSimmableItems() map[int32]*proto.UIItem {
+	return core.FilterMap(db.items, func(_ int32, item *proto.UIItem) bool {
+		if _, ok := itemAllowList[item.Id]; ok {
+			return true
 		}
 
-		if _, ok := itemAllowList[itemData.Override.ID]; ok {
-			included = append(included, itemData)
-			continue
-		}
-
-		itemLevel := itemData.Response.GetItemLevel()
-		qual := itemData.Response.GetQuality()
-		if qual < int(proto.ItemQuality_ItemQualityUncommon) {
-			continue
-		} else if qual > int(proto.ItemQuality_ItemQualityLegendary) {
-			continue
-		} else if qual < int(proto.ItemQuality_ItemQualityEpic) {
-			if itemLevel < 145 {
-				continue
+		if item.Quality < proto.ItemQuality_ItemQualityUncommon {
+			return false
+		} else if item.Quality > proto.ItemQuality_ItemQualityLegendary {
+			return false
+		} else if item.Quality < proto.ItemQuality_ItemQualityEpic {
+			if item.Ilvl < 145 {
+				return false
 			}
-			if itemLevel < 149 && itemData.Response.GetItemSetName() == "" {
-				continue
+			if item.Ilvl < 149 && item.SetName == "" {
+				return false
 			}
 		} else {
 			// Epic and legendary items might come from classic, so use a lower ilvl threshold.
-			if itemLevel < 140 {
-				continue
+			if item.Ilvl < 140 {
+				return false
 			}
 		}
-		if itemLevel == 0 {
-			fmt.Printf("Missing ilvl: %s\n", itemData.Response.GetName())
+		if item.Ilvl == 0 {
+			fmt.Printf("Missing ilvl: %s\n", item.Name)
 		}
 
-		included = append(included, itemData)
-	}
-
-	return included
+		return true
+	})
 }
 
 // Returns only gems which are worth including in the sim.
@@ -249,12 +193,16 @@ func (db *WowDatabase) toUIDatabase() *proto.UIDatabase {
 		SpellIcons: db.spellIcons,
 	}
 
-	for _, itemData := range db.getSimmableItems() {
-		uiDB.Items = append(uiDB.Items, itemData.toProto())
+	for _, item := range db.getSimmableItems() {
+		uiDB.Items = append(uiDB.Items, item)
 	}
 	for _, gem := range db.getSimmableGems() {
 		uiDB.Gems = append(uiDB.Gems, gem)
 	}
+
+	slices.SortStableFunc(uiDB.Items, func(i1, i2 *proto.UIItem) bool {
+		return i1.Id < i2.Id
+	})
 	slices.SortStableFunc(uiDB.Gems, func(g1, g2 *proto.UIGem) bool {
 		return g1.Id < g2.Id
 	})
