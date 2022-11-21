b2b03407967c0f1091f0416870468058b3a91041
James Tanner <jimmyt857@gmail.com>
2022-11-20 21:02:22 -0800

    A bit of organizing in db generator

--- tools/generate_items/main.go
+++ tools/generate_items/main.go
@@ -19,5 +19,6 @@ func main() {
 		getWowheadTooltipsDB("./assets/item_data/all_item_tooltips.csv"),
 		getWowheadTooltipsDB("./assets/spell_data/all_spell_tooltips.csv"))
 
+	db.applyGlobalFilters()
 	writeDatabaseFile(db)
 }
