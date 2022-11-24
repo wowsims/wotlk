85548781963e23edd032a9743194e0ca47655692
Kayla Glick <12898988+kayla-glick@users.noreply.github.com>
2022-11-24 15:51:40 +0000

    fix bootstrap and Popper imports

--- ui/index.ts
+++ ui/index.ts
@@ -1,4 +1,14 @@
 import * as Popper from '@popperjs/core';
 import * as bootstrap from 'bootstrap';
 
+declare global {
+  interface Window {
+    Popper: any;
+    bootstrap: any;
+  }
+}
+
+window.Popper = Popper;
+window.bootstrap = bootstrap;
+
 import './shared/bootstrap_overrides';
