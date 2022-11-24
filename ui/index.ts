c78b2c01d00462f5fac51bb9ace1023207a7c2d9
Kayla Glick <12898988+kayla-glick@users.noreply.github.com>
2022-11-24 16:08:55 +0000

    prevent popping on page load

--- ui/index.ts
+++ ui/index.ts
@@ -12,3 +12,17 @@ window.Popper = Popper;
 window.bootstrap = bootstrap;
 
 import './shared/bootstrap_overrides';
+
+function docReady(fn: any) {
+  // see if DOM is already available
+  if (document.readyState === "complete" || document.readyState === "interactive") {
+      // call on next available tick
+      setTimeout(fn, 1);
+  } else {
+      document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+docReady(function() {
+  document.getElementsByTagName('body')[0].classList.add('ready');
+});
