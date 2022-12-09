b70cef25b85304acff2e0e50f634391a6ece4482
jarves <jarveson@gmail.com>
2022-12-08 18:09:30 -0600

    ui/feral: remove 'humanoid' stats displayed warning
    Signed-off-by: jarves <jarveson@gmail.com>

--- ui/feral_druid/sim.ts
+++ ui/feral_druid/sim.ts
@@ -22,7 +22,6 @@ export class FeralDruidSimUI extends IndividualSimUI<Spec.SpecFeralDruid> {
 			cssClass: 'feral-druid-sim-ui',
 			// List any known bugs / issues here and they'll be shown on the site.
 			knownIssues: [
-				"Stats display only humanoid form"
 			],
 			warnings: [
 			],
