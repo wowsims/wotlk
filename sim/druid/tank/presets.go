4fc0c6caba8175d47d942811778f9fe710b61a49
NerdEgghead <nerdeggheadtbc@gmail.com>
2022-12-16 16:25:59 -0800

    Updated Feral tank testing preset to WotLK defaults.
     Changes to be committed:
    	modified:   sim/druid/tank/TestFeralTank.results
    	modified:   sim/druid/tank/presets.go

--- sim/druid/tank/presets.go
+++ sim/druid/tank/presets.go
@@ -7,23 +7,34 @@ import (
 
 var StandardTalents = &proto.DruidTalents{
 	Ferocity:                5,
+	FeralInstinct:           3,
+	SavageFury:              2,
+	ThickHide:               3,
+	FeralSwiftness:          2,
+	SurvivalInstincts:       true,
 	SharpenedClaws:          3,
 	ShreddingAttacks:        2,
 	PredatoryStrikes:        3,
 	PrimalFury:              2,
-	SavageFury:              2,
+	PrimalPrecision:         2,
+	FeralCharge:             true,
+	NaturalReaction:         3,
 	HeartOfTheWild:          5,
 	SurvivalOfTheFittest:    3,
 	LeaderOfThePack:         true,
 	ImprovedLeaderOfThePack: 2,
-	PredatoryInstincts:      3,
+	ProtectorOfThePack:      3,
+	KingOfTheJungle:         3,
 	Mangle:                  true,
+	ImprovedMangle:          3,
+	RendAndTear:             5,
+	PrimalGore:              true,
+	Berserk:                 true,
 
-	Furor:               5,
-	Naturalist:          5,
-	NaturalShapeshifter: 3,
-	Intensity:           3,
-	OmenOfClarity:       true,
+	ImprovedMarkOfTheWild: 2,
+	Furor:                 3,
+	Naturalist:            5,
+	OmenOfClarity:         true,
 }
 
 var PlayerOptionsDefault = &proto.Player_FeralTankDruid{
@@ -34,112 +45,111 @@ var PlayerOptionsDefault = &proto.Player_FeralTankDruid{
 			StartingRage:    20,
 		},
 		Rotation: &proto.FeralTankDruid_Rotation{
-			MaulRageThreshold:        60,
+			MaulRageThreshold:        25,
 			MaintainDemoralizingRoar: true,
-			LacerateTime:             5.0,
+			LacerateTime:             8.0,
 		},
 	},
 }
 
 var FullConsumes = &proto.Consumes{
-	BattleElixir:    proto.BattleElixir_ElixirOfMajorAgility,
-	Food:            proto.Food_FoodGrilledMudfish,
-	DefaultPotion:   proto.Potions_HastePotion,
-	DefaultConjured: proto.Conjured_ConjuredDarkRune,
+	BattleElixir:    proto.BattleElixir_GurusElixir,
+	GuardianElixir:  proto.GuardianElixir_GiftOfArthas,
+	Food:            proto.Food_FoodBlackenedDragonfin,
+	DefaultPotion:   proto.Potions_IndestructiblePotion,
+	DefaultConjured: proto.Conjured_ConjuredHealthstone,
+	ThermalSapper:   true,
+	FillerExplosive: proto.Explosive_ExplosiveSaroniteBomb,
 }
 
 var P1Gear = core.EquipmentSpecFromJsonString(`{"items": [
-	{
-		"id": 34404,
-		"enchant": 3004,
-		"gems": [
-			32212,
-			25896
-		]
-	},
-	{
-		"id": 34178
-	},
-	{
-		"id": 34392,
-		"enchant": 2991,
-		"gems": [
-			32200,
-			32200
-		]
-	},
-	{
-		"id": 34190,
-		"enchant": 368
-	},
-	{
-		"id": 34211,
-		"enchant": 2661,
-		"gems": [
-			32200,
-			32200,
-			32200
-		]
-	},
-	{
-		"id": 34444,
-		"enchant": 2649,
-		"gems": [
-			32200,
-			0
-		]
-	},
-	{
-		"id": 34408,
-		"enchant": 2613,
-		"gems": [
-			32200,
-			32200,
-			0
-		]
-	},
-	{
-		"id": 35156,
-		"gems": [
-			0
-		]
-	},
-	{
-		"id": 34385,
-		"enchant": 3013,
-		"gems": [
-			32200,
-			32200,
-			32200
-		]
-	},
-	{
-		"id": 34573,
-		"enchant": 2940,
-		"gems": [
-			32200
-		]
-	},
-	{
-		"id": 34213,
-		"enchant": 2931
-	},
-	{
-		"id": 34361,
-		"enchant": 2931
-	},
-	{
-		"id": 32501
-	},
-	{
-		"id": 32658
-	},
-	{
-		"id": 30883,
-		"enchant": 2670
-	},
-	{},
-	{
-		"id": 32387
-	}
+		{
+			"id": 40329,
+			"enchant": 3878,
+			"gems": [
+				41339,
+				40008
+			]
+		},
+		{
+			"id": 40387
+		},
+		{
+			"id": 40494,
+			"enchant": 3852,
+			"gems": [
+				40008
+			]
+		},
+		{
+			"id": 40252,
+			"enchant": 3294
+		},
+		{
+			"id": 40471,
+			"enchant": 3832,
+			"gems": [
+				42702,
+				40088
+			]
+		},
+		{
+			"id": 40186,
+			"enchant": 3850,
+			"gems": [
+				40008,
+				0
+			]
+		},
+		{
+			"id": 40472,
+			"enchant": 3860,
+			"gems": [
+				40008,
+				0
+			]
+		},
+		{
+			"id": 43591,
+			"gems": [
+				40008,
+				40008,
+				40008
+			]
+		},
+		{
+			"id": 44011,
+			"enchant": 3822,
+			"gems": [
+				40008,
+				40008
+			]
+		},
+		{
+			"id": 40243,
+			"enchant": 3606,
+			"gems": [
+				40008
+			]
+		},
+		{
+			"id": 40370
+		},
+		{
+			"id": 37784
+		},
+		{
+			"id": 44253
+		},
+		{
+			"id": 37220
+		},
+		{
+			"id": 40280,
+			"enchant": 2673
+		},
+		{},
+		{
+			"id": 38365
+		}
 ]}`)
