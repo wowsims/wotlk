836ea887f2902dca596286a0f7d64a8dda39dd3b
James Tanner <jimmyt857@gmail.com>
2023-01-26 00:11:26 -0500

    Remove more ModifyCasts

--- sim/warrior/warrior.go
+++ sim/warrior/warrior.go
@@ -34,14 +34,13 @@ type Warrior struct {
 	WarriorInputs
 
 	// Current state
-	Stance                 Stance
-	overpowerValidUntil    time.Duration
-	rendValidUntil         time.Duration
-	shoutExpiresAt         time.Duration
-	revengeProcAura        *core.Aura
-	glyphOfRevengeProcAura *core.Aura
-	lastTasteForBloodProc  time.Duration
-	Ymirjar4pcProcAura     *core.Aura
+	Stance                Stance
+	overpowerValidUntil   time.Duration
+	rendValidUntil        time.Duration
+	shoutExpiresAt        time.Duration
+	revengeProcAura       *core.Aura
+	lastTasteForBloodProc time.Duration
+	Ymirjar4pcProcAura    *core.Aura
 
 	munchedDeepWoundsProcs []*core.PendingAction
 
@@ -90,10 +89,9 @@ type Warrior struct {
 	DefensiveStanceAura *core.Aura
 	BerserkerStanceAura *core.Aura
 
-	BloodsurgeAura    *core.Aura
-	SuddenDeathAura   *core.Aura
-	SwordAndBoardAura *core.Aura
-	ShieldBlockAura   *core.Aura
+	BloodsurgeAura  *core.Aura
+	SuddenDeathAura *core.Aura
+	ShieldBlockAura *core.Aura
 
 	DemoralizingShoutAuras []*core.Aura
 	BloodFrenzyAuras       []*core.Aura
