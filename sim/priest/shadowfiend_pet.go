b132a265da98fe67cba7588fee3d44ca8632e8cc
CyberQuack <cyber.quack.games@gmail.com>
2023-03-07 00:51:46 -0800

    Fix priest multi shadowfiend damage

--- sim/priest/shadowfiend_pet.go
+++ sim/priest/shadowfiend_pet.go
@@ -69,7 +69,7 @@ func (priest *Priest) NewShadowfiend() *Shadowfiend {
 			BaseDamageMax:        210,
 			SwingSpeed:           1.5,
 			NormalizedSwingSpeed: 1.5,
-			SwingDuration:        time.Millisecond * 1500,
+			SwingDuration:        time.Second,
 			CritMultiplier:       2,
 			SpellSchool:          core.SpellSchoolShadow,
 		},
@@ -122,6 +122,10 @@ func (shadowfiend *Shadowfiend) OnGCDReady(sim *core.Simulation) {
 func (shadowfiend *Shadowfiend) Reset(sim *core.Simulation) {
 	shadowfiend.ShadowcrawlAura.Deactivate(sim)
 	shadowfiend.Disable(sim)
+}
+
+func (shadowfiend *Shadowfiend) OnPetDisable(sim *core.Simulation) {
+	shadowfiend.ShadowcrawlAura.Deactivate(sim)
 }
 
 func (shadowfiend *Shadowfiend) GetPet() *core.Pet {
