wowsims/wotlk

commit 405abda03b0040c8bccdb795069d181a58fba837

warlock: implement weapon swapping support
Fixes #2221.

ui/core/components/input_helpers.ts
@@ -493,6 +493,7 @@ export interface TypedItemSwapPickerConfig<SpecType extends Spec, T> extends Ite
 interface WrappedItemSwapInputConfig<SpecType extends Spec> {
 	fieldName: keyof SpecRotation<SpecType>,
 	values: Array<ItemSlot>,
+	label: string,
 	labelTooltip?: string,
 	getValue?: (player: Player<SpecType>) => ItemSwap,
 	setValue?: (eventID: EventID, player: Player<SpecType>, newValue: ItemSwap) => void,
@@ -510,6 +511,7 @@ export function MakeItemSwapInput<SpecType extends Spec>(config: WrappedItemSwap
 		}),
 		itemSlots: config.values,
 		changedEvent: (player: Player<SpecType>) => player.rotationChangeEmitter,
+		label: config.label,
 		labelTooltip: config.labelTooltip,
 		showWhen: config.showWhen,
 	}

ui/deathknight/inputs.ts
@@ -382,6 +382,7 @@ export const WeaponSwapInputs = InputHelpers.MakeItemSwapInput<Spec.SpecDeathkni
 		ItemSlot.ItemSlotOffHand,
 		//ItemSlot.ItemSlotRanged, Not support yet
 	],
+	label: 'Item Swap',
 	labelTooltip: '<b>Berserking</b> will be equipped when FC has procced and Berserking is not active.<br><br><b>Black Magic</b> will be prioed to swap during gargoyle or if gargoyle will be on CD for full BM Icd.',
 	showWhen: (player: Player<Spec.SpecDeathknight>) => player.getTalents().summonGargoyle && player.getRotation().useGargoyle && player.getRotation().enableWeaponSwap,
 })

ui/enhancement_shaman/inputs.ts
@@ -90,6 +90,7 @@ export const EnhancmentItemSwapInputs = InputHelpers.MakeItemSwapInput<Spec.Spec
 		ItemSlot.ItemSlotOffHand,
 		//ItemSlot.ItemSlotRanged, Not support yet
 	],
+	label: 'Item Swap',
 	labelTooltip: 'Start with the swapped items until Fire Elemntal has been summoned, swap back to normal gear set. Weapons come pre enchanted with FT9 and FT10. If a slot is empty it will not be used in the swap',
 	showWhen: (player: Player<Spec.SpecEnhancementShaman>) => (player.getRotation().totems?.useFireElemental && player.getRotation().enableItemSwap) || false
 })

ui/warlock/inputs.ts
@@ -11,7 +11,7 @@ import {
 	Warlock_Options_Summon as Summon,
 } from '../core/proto/warlock.js';
 
-import { RaidTarget, Spec, Glyphs, Debuffs, IndividualBuffs, RaidBuffs } from '../core/proto/common.js';
+import { RaidTarget, Spec, Glyphs, Debuffs, IndividualBuffs, RaidBuffs, ItemSwap, ItemSlot } from '../core/proto/common.js';
 import { NO_TARGET } from '../core/proto_utils/utils.js';
 import { ActionId } from '../core/proto_utils/action_id.js';
 import { Player } from '../core/player.js';
@@ -142,7 +142,6 @@ export const CorruptionSpell = {
 		player.setRotation(eventID, newRotation);
 	},
 };
-
 
 export const WarlockRotationConfig = {
 	inputs: [
@@ -302,6 +301,23 @@ export const WarlockRotationConfig = {
 			label: 'Detonate Seed on Cast',
 			labelTooltip: 'Simulates raid doing damage to targets such that seed detonates immediately on cast.',
 			showWhen: (player: Player<Spec.SpecWarlock>) => player.getRotation().primarySpell == PrimarySpell.Seed,
+		}),
+		InputHelpers.makeRotationBooleanInput<Spec.SpecWarlock>({
+			fieldName: 'enableWeaponSwap',
+			label: 'Enable Weapon Swapping',
+			labelTooltip: 'Toggle on/off item swapping',
+			showWhen: (player: Player<Spec.SpecWarlock>) => player.getRotation().type == RotationType.Affliction
+		}),
+		InputHelpers.MakeItemSwapInput<Spec.SpecWarlock>({
+			fieldName: 'weaponSwap',
+			label: 'Weapon Swap',
+			values: [
+				ItemSlot.ItemSlotMainHand,
+				ItemSlot.ItemSlotOffHand,
+				ItemSlot.ItemSlotRanged,
+			],
+			labelTooltip: 'Start with the swapped items until Corruption has been cast, then swap back to normal gear set. If a slot is empty it will not be used in the swap',
+			showWhen: (player: Player<Spec.SpecWarlock>) => (player.getRotation().type == RotationType.Affliction && player.getRotation().enableWeaponSwap) || false
 		}),
 	],
 };

proto/warlock.proto
@@ -2,6 +2,7 @@ syntax = "proto3";
 package proto;
 
 option go_package = "./proto";
+import "common.proto";
 
 // WotLK talents
 message WarlockTalents {
@@ -170,7 +171,7 @@ message Warlock {
 			ChaosBolt = 1;
 			Haunt = 2;
 		}
-		Preset preset= 1;
+		Preset preset = 1;
 		Curse curse = 2;
 		PrimarySpell primary_spell = 3;
 		SecondaryDot secondary_dot = 4;
@@ -179,6 +180,9 @@ message Warlock {
 		SpecSpell spec_spell = 7;
 		Type type = 8;
 		bool use_infernal = 9;
+
+		bool enable_weapon_swap = 10;
+		ItemSwap weapon_swap = 11;
 	}
 	Rotation rotation = 1;
 	

sim/core/item_swaps.go
@@ -97,6 +97,17 @@ func (swap *ItemSwap) GetItem(slot proto.ItemSlot) *Item {
 	return &swap.unEquippedItems[slot-offset]
 }
 
+func (swap *ItemSwap) CalcStatChanges(slots []proto.ItemSlot) stats.Stats {
+	newStats := stats.Stats{}
+	for _, slot := range slots {
+		oldItemStats := swap.getItemStats(swap.character.Equip[slot])
+		newItemStats := swap.getItemStats(*swap.GetItem(slot))
+		newStats = newStats.Add(newItemStats.Subtract(oldItemStats))
+	}
+
+	return newStats
+}
+
 func (swap *ItemSwap) SwapItems(sim *Simulation, slots []proto.ItemSlot, useGCD bool) {
 	if !swap.IsEnabled() {
 		return

sim/warlock/TestAffliction.results
@@ -310,6 +310,48 @@ dps_results: {
  }
 }
 dps_results: {
+ key: "TestAffliction-Settings-Orc-P2-AffItemSwap-FullBuffs-LongMultiTarget"
+ value: {
+  dps: 9917.93286
+  tps: 10635.38151
+ }
+}
+dps_results: {
+ key: "TestAffliction-Settings-Orc-P2-AffItemSwap-FullBuffs-LongSingleTarget"
+ value: {
+  dps: 9917.93286
+  tps: 8921.08721
+ }
+}
+dps_results: {
+ key: "TestAffliction-Settings-Orc-P2-AffItemSwap-FullBuffs-ShortSingleTarget"
+ value: {
+  dps: 10579.33103
+  tps: 9539.13517
+ }
+}
+dps_results: {
+ key: "TestAffliction-Settings-Orc-P2-AffItemSwap-NoBuffs-LongMultiTarget"
+ value: {
+  dps: 5681.69666
+  tps: 7350.03451
+ }
+}
+dps_results: {
+ key: "TestAffliction-Settings-Orc-P2-AffItemSwap-NoBuffs-LongSingleTarget"
+ value: {
+  dps: 5681.69666
+  tps: 5345.70269
+ }
+}
+dps_results: {
+ key: "TestAffliction-Settings-Orc-P2-AffItemSwap-NoBuffs-ShortSingleTarget"
+ value: {
+  dps: 5495.32631
+  tps: 5085.13094
+ }
+}
+dps_results: {
  key: "TestAffliction-Settings-Orc-P2-Affliction Warlock-FullBuffs-LongMultiTarget"
  value: {
   dps: 9897.64989

sim/warlock/rotation.go
@@ -545,6 +545,14 @@ func (warlock *Warlock) OnGCDReady(sim *core.Simulation) {
 
 		if success := ac.Spell.Cast(sim, target); success {
 			if !warlock.GCD.IsReady(sim) {
+				// after-GCD actions
+
+				if ac.Spell == warlock.Corruption && warlock.ItemSwap.IsEnabled() && warlock.swapped {
+					warlock.ItemSwap.SwapItems(sim, []proto.ItemSlot{proto.ItemSlot_ItemSlotMainHand,
+						proto.ItemSlot_ItemSlotOffHand, proto.ItemSlot_ItemSlotRanged}, true)
+					warlock.swapped = false
+				}
+
 				return
 			}
 

commit dafa64346d9258bf8b9468886f2f2cf2e54bf8a3

core: fix SwapItem label bug introduced in 405abda

ui/core/components/input_helpers.ts
@@ -493,7 +493,6 @@ export interface TypedItemSwapPickerConfig<SpecType extends Spec, T> extends Ite
 interface WrappedItemSwapInputConfig<SpecType extends Spec> {
 	fieldName: keyof SpecRotation<SpecType>,
 	values: Array<ItemSlot>,
-	label: string,
 	labelTooltip?: string,
 	getValue?: (player: Player<SpecType>) => ItemSwap,
 	setValue?: (eventID: EventID, player: Player<SpecType>, newValue: ItemSwap) => void,
@@ -511,7 +510,6 @@ export function MakeItemSwapInput<SpecType extends Spec>(config: WrappedItemSwap
 		}),
 		itemSlots: config.values,
 		changedEvent: (player: Player<SpecType>) => player.rotationChangeEmitter,
-		label: config.label,
 		labelTooltip: config.labelTooltip,
 		showWhen: config.showWhen,
 	}

ui/deathknight/inputs.ts
@@ -382,7 +382,6 @@ export const WeaponSwapInputs = InputHelpers.MakeItemSwapInput<Spec.SpecDeathkni
 		ItemSlot.ItemSlotOffHand,
 		//ItemSlot.ItemSlotRanged, Not support yet
 	],
-	label: 'Item Swap',
 	labelTooltip: '<b>Berserking</b> will be equipped when FC has procced and Berserking is not active.<br><br><b>Black Magic</b> will be prioed to swap during gargoyle or if gargoyle will be on CD for full BM Icd.',
 	showWhen: (player: Player<Spec.SpecDeathknight>) => player.getTalents().summonGargoyle && player.getRotation().useGargoyle && player.getRotation().enableWeaponSwap,
 })

ui/enhancement_shaman/inputs.ts
@@ -90,7 +90,6 @@ export const EnhancmentItemSwapInputs = InputHelpers.MakeItemSwapInput<Spec.Spec
 		ItemSlot.ItemSlotOffHand,
 		//ItemSlot.ItemSlotRanged, Not support yet
 	],
-	label: 'Item Swap',
 	labelTooltip: 'Start with the swapped items until Fire Elemntal has been summoned, swap back to normal gear set. Weapons come pre enchanted with FT9 and FT10. If a slot is empty it will not be used in the swap',
 	showWhen: (player: Player<Spec.SpecEnhancementShaman>) => (player.getRotation().totems?.useFireElemental && player.getRotation().enableItemSwap) || false
 })

ui/warlock/inputs.ts
@@ -275,7 +275,6 @@ export const WarlockRotationConfig = {
 		}),
 		InputHelpers.MakeItemSwapInput<Spec.SpecWarlock>({
 			fieldName: 'weaponSwap',
-			label: 'Weapon Swap',
 			values: [
 				ItemSlot.ItemSlotMainHand,
 				ItemSlot.ItemSlotOffHand,