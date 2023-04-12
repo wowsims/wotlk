00d97b168235182798f8bfa64a0059697499979c
jarves <jarveson@gmail.com>
2023-04-12 13:55:39 -0500

    feral: Prepop ooc doesnt need fff anymore  - also revert gotw func change, even if still unused
    Signed-off-by: jarves <jarveson@gmail.com>

--- ui/feral_druid/inputs.ts
+++ ui/feral_druid/inputs.ts
@@ -44,7 +44,7 @@ export const LatencyMs = InputHelpers.makeSpecOptionsNumberInput<Spec.SpecFeralD
 export const PrepopOoc = InputHelpers.makeSpecOptionsBooleanInput<Spec.SpecFeralDruid>({
 	fieldName: 'prepopOoc',
 	label: 'Pre-pop Clearcasting',
-	labelTooltip: 'Start fight with clearcasting using FFF',
+	labelTooltip: 'Start fight with clearcasting',
 	showWhen: (player: Player<Spec.SpecFeralDruid>) => player.getTalents().omenOfClarity,
 	changeEmitter: (player: Player<Spec.SpecFeralDruid>) => TypedEvent.onAny([player.rotationChangeEmitter, player.talentsChangeEmitter]),
 })
