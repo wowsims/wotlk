ba084fb89f4c8bbd0f8e6bb9feaa87db38900832
vigo <v.harrach@sealmedia.de>
2023-04-14 20:09:16 +0200

    [rogue] add "Garona's Battlegear" as alternative name for "VanCleef's Battlegear" [rogue] rename all tier sets to "TierX", and Gladiator's Vestments to "Arena" [rogue] Tier9-4pc doesn't wrongly apply to Ghostly Strike and Ambush anymore

--- sim/rogue/backstab.go
+++ sim/rogue/backstab.go
@@ -30,7 +30,7 @@ func (rogue *Rogue) registerBackstabSpell() {
 			return !rogue.PseudoStats.InFrontOfTarget && rogue.GetMHWeapon().WeaponType == proto.WeaponType_WeaponTypeDagger
 		},
 
-		BonusCritRating: core.TernaryFloat64(rogue.HasSetBonus(ItemSetVanCleefs, 4), 5*core.CritRatingPerCritChance, 0) +
+		BonusCritRating: core.TernaryFloat64(rogue.HasSetBonus(Tier9, 4), 5*core.CritRatingPerCritChance, 0) +
 			[]float64{0, 2, 4, 6}[rogue.Talents.TurnTheTables]*core.CritRatingPerCritChance +
 			10*core.CritRatingPerCritChance*float64(rogue.Talents.PuncturingWounds),
 		// All of these use "Apply Aura: Modifies Damage/Healing Done", and stack additively (up to 142%).
@@ -40,7 +40,7 @@ func (rogue *Rogue) registerBackstabSpell() {
 			0.03*float64(rogue.Talents.Aggression) +
 			0.05*float64(rogue.Talents.BladeTwisting) +
 			core.TernaryFloat64(rogue.Talents.SurpriseAttacks, 0.1, 0) +
-			core.TernaryFloat64(rogue.HasSetBonus(ItemSetSlayers, 4), 0.06, 0)) *
+			core.TernaryFloat64(rogue.HasSetBonus(Tier6, 4), 0.06, 0)) *
 			(1 + 0.02*float64(rogue.Talents.SinisterCalling)),
 		CritMultiplier:   rogue.MeleeCritMultiplier(true),
 		ThreatMultiplier: 1,
