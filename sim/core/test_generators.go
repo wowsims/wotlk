1cdcad3db1ce081d9546423cf57a94f5eb16964d
zku <ochmsku@gmail.com>
2023-04-30 17:45:48 +0200

    update test code to propagate complete player data

--- sim/core/test_generators.go
+++ sim/core/test_generators.go
@@ -112,6 +112,7 @@ type SettingsCombos struct {
 	Encounters  []EncounterCombo
 	SimOptions  *proto.SimOptions
 	IsHealer    bool
+	Cooldowns   *proto.Cooldowns
 }
 
 func (combos *SettingsCombos) NumTests() int {
@@ -169,6 +170,7 @@ func (combos *SettingsCombos) GetTest(testIdx int) (string, *proto.ComputeStatsR
 				Consumes:      buffsCombo.Consumes,
 				Buffs:         buffsCombo.Player,
 				Profession1:   proto.Profession_Engineering,
+				Cooldowns:     combos.Cooldowns,
 			}, specOptionsCombo.SpecOptions),
 			buffsCombo.Party,
 			buffsCombo.Raid,
@@ -549,6 +551,7 @@ func FullCharacterTestSuiteGenerator(config CharacterSuiteConfig) TestGenerator 
 					IsHealer:   config.IsHealer,
 					Encounters: MakeDefaultEncounterCombos(),
 					SimOptions: DefaultSimTestOptions,
+					Cooldowns:  config.Cooldowns,
 				},
 			},
 			{
