e3b2b7ad19e5b99b058ef0b72ceb2444ff812768
rosenrusinov <rainisunbelievable@gmail.com>
2023-07-13 14:36:42 +0200

    update frost preset

--- ui/mage/presets.ts
+++ ui/mage/presets.ts
@@ -270,8 +270,11 @@ export const FROST_ROTATION_PRESET_DEFAULT = {
 			],
 			"priorityList": [
 			  {"action":{"autocastOtherCooldowns":{}}},
+			  {"action":{"condition":{"not":{"val":{"auraIsActive":{"auraId":{"spellId":12472}}}}},"castSpell":{"spellId":{"spellId":26297}}}},
+			  {"action":{"condition":{"not":{"val":{"auraIsActive":{"auraId":{"spellId":12472}}}}},"castSpell":{"spellId":{"spellId":54758}}}},
+			  {"action":{"condition":{"cmp":{"op":"OpLe","lhs":{"currentManaPercent":{}},"rhs":{"const":{"val":"25%"}}}},"castSpell":{"spellId":{"spellId":12051}}}},
 			  {"action":{"condition":{"auraIsActive":{"auraId":{"spellId":44545}}},"castSpell":{"spellId":{"spellId":44572}}}},
-			  {"action":{"condition":{"and":{"vals":[{"auraIsActive":{"auraId":{"spellId":44545}}},{"auraIsActive":{"auraId":{"spellId":44549}}}]}},"castSpell":{"spellId":{"spellId":47610}}}},
+			  {"action":{"condition":{"auraIsActive":{"auraId":{"spellId":44549}}},"castSpell":{"spellId":{"spellId":47610}}}},
 			  {"action":{"castSpell":{"spellId":{"spellId":42842}}}}
 			]
 		}`),
