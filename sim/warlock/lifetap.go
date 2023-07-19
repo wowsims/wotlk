c6a8c3749cebfeb3e146748cffb898d0a7e2bba7
Tharre <tharre3@gmail.com>
2023-07-19 23:47:33 +0200

    warlock: fix mana feed crash when missing pet
    Fixes #3324.

--- sim/warlock/lifetap.go
+++ sim/warlock/lifetap.go
@@ -11,7 +11,7 @@ func (warlock *Warlock) registerLifeTapSpell() {
 	manaMetrics := warlock.NewManaMetrics(actionID)
 
 	var petManaMetrics *core.ResourceMetrics
-	if warlock.Talents.ManaFeed {
+	if warlock.Talents.ManaFeed && warlock.Pet != nil {
 		petManaMetrics = warlock.Pet.NewManaMetrics(actionID)
 	}
 
@@ -32,7 +32,7 @@ func (warlock *Warlock) registerLifeTapSpell() {
 			restore := (2000.0 + 0.5*warlock.GetStat(stats.SpellPower)) * impLifetap
 			warlock.AddMana(sim, restore, manaMetrics)
 
-			if warlock.Talents.ManaFeed {
+			if warlock.Talents.ManaFeed && warlock.Pet != nil {
 				warlock.Pet.AddMana(sim, restore, petManaMetrics)
 			}
 			if warlock.GlyphOfLifeTapAura != nil {
