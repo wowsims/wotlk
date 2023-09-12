b8283c8729945e44193d4d7f611f187c68c69352
Josh <jmarsiglio@gmail.com>
2023-09-12 13:57:12 +0200

    break up evocate into ticks (#3664)
    * break up evocate into ticks
    
    * update APLs

--- sim/mage/evocation.go
+++ sim/mage/evocation.go
@@ -8,27 +8,18 @@ import (
 
 const EvocationId = 12051
 
-func (mage *Mage) registerEvocationCD() {
+func (mage *Mage) registerEvocation(numTicks int32) *core.Spell {
 	actionID := core.ActionID{SpellID: EvocationId}
 	manaMetrics := mage.NewManaMetrics(actionID)
 
-	maxTicks := core.TernaryInt32(mage.HasSetBonus(ItemSetTempestRegalia, 2), 5, 4)
-
-	numTicks := core.MaxInt32(0, core.MinInt32(maxTicks, mage.Options.EvocationTicks))
-	if numTicks == 0 {
-		numTicks = maxTicks
-	}
-
 	channelTime := time.Duration(numTicks) * time.Second * 2
 	manaPerTick := 0.0
-	manaThreshold := 0.0
 	mage.Env.RegisterPostFinalizeEffect(func() {
 		manaPerTick = mage.MaxMana() * 0.15
-		manaThreshold = mage.MaxMana() * 0.3
 	})
 
-	evocationSpell := mage.RegisterSpell(core.SpellConfig{
-		ActionID: actionID,
+	return mage.GetOrRegisterSpell(core.SpellConfig{
+		ActionID: actionID.WithTag(numTicks),
 		Flags:    core.SpellFlagAPL,
 
 		Cast: core.CastConfig{
@@ -53,6 +44,22 @@ func (mage *Mage) registerEvocationCD() {
 			})
 		},
 	})
+}
+
+func (mage *Mage) registerEvocationCD() {
+	maxTicks := core.TernaryInt32(mage.HasSetBonus(ItemSetTempestRegalia, 2), 5, 4)
+
+	numTicks := core.MaxInt32(0, core.MinInt32(maxTicks, mage.Options.EvocationTicks))
+	if numTicks == 0 {
+		numTicks = maxTicks
+	}
+
+	manaThreshold := 0.0
+	mage.Env.RegisterPostFinalizeEffect(func() {
+		manaThreshold = mage.MaxMana() * 0.3
+	})
+
+	evocationSpell := mage.registerEvocation(numTicks)
 
 	mage.AddMajorCooldown(core.MajorCooldown{
 		Spell: evocationSpell,
@@ -71,4 +78,12 @@ func (mage *Mage) registerEvocationCD() {
 			return curMana < manaThreshold
 		},
 	})
+}
+
+func (mage *Mage) registerEvocationSpells() {
+	maxTicks := core.TernaryInt32(mage.HasSetBonus(ItemSetTempestRegalia, 2), 5, 4)
+
+	for i := int32(1); i <= maxTicks; i++ {
+		mage.registerEvocation(i)
+	}
 }
