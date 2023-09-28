08e234a5588f1092f012535b35a32df2c6e40a85
Adrian Klingen <adrian.klingen@deptagency.com>
2023-09-29 00:26:16 +0200

    Add disable expertise gemming option to warrior sim

--- ui/warrior/inputs.ts
+++ ui/warrior/inputs.ts
@@ -38,6 +38,19 @@ export const StartingRage = InputHelpers.makeSpecOptionsNumberInput<Spec.SpecWar
 	labelTooltip: 'Initial rage at the start of each iteration.',
 });
 
+// Allows for auto gemming whilst ignoring expertise cap
+// (Useful for Arms)
+export const DisableExpertiseGemming = {
+	type: 'boolean' as const,
+	label: 'Disable expertise gemming',
+	labelTooltip: 'Disables auto gemming for expertise',
+	changedEvent: (player: Player<any>) => player.disableExpertiseGemmingChangeEmitter,
+	getValue: (player: Player<any>) => player.getDisableExpertiseGemming(),
+	setValue: (eventID: EventID, player: Player<any>, newValue: boolean) => {
+		player.setDisableExpertiseGemming(eventID, newValue);
+	},
+};
+
 export const StanceSnapshot = InputHelpers.makeSpecOptionsBooleanInput<Spec.SpecWarrior>({
 	fieldName: 'stanceSnapshot',
 	label: 'Stance Snapshot',
