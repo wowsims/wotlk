14b40bca54fd5090b10f2fecf0ebe75fbd1733cd
rosenrusinov <rainisunbelievable@gmail.com>
2023-12-12 10:24:19 +0100

    add more enchants and wlock firestone

--- sim/warlock/talents.go
+++ sim/warlock/talents.go
@@ -27,20 +27,89 @@ func (warlock *Warlock) applyImprovedShadowBolt() {
 
 func (warlock *Warlock) applyWeaponImbue() {
 	level := warlock.GetCharacter().Level
-	// TODO: Classic warlock firestone + fire damage on hit
 	if warlock.Options.WeaponImbue == proto.Warlock_Options_Firestone {
-		if level >= 56 {
-			warlock.AddStat(stats.FirePower, 21)
-		} else if level >= 36 {
-			warlock.AddStat(stats.FirePower, 14)
-		} else if level >= 28 {
-			warlock.AddStat(stats.FirePower, 10)
-		}
+		warlock.applyFirestone()
 	}
 	if warlock.Options.WeaponImbue == proto.Warlock_Options_Spellstone {
 		if level >= 55 {
 			warlock.AddStat(stats.SpellCrit, 1*core.SpellCritRatingPerCritChance)
 		}
+	}
+}
+
+func (warlock *Warlock) applyFirestone() {
+	level := warlock.GetCharacter().Level
+
+	damageMin := 0.0
+	damageMax := 0.0
+
+	// TODO: Test for spell scaling
+	spellCoeff := 0.0
+	spellId := int32(0)
+
+	// TODO: Test PPM
+	ppm := warlock.AutoAttacks.NewPPMManager(8, core.ProcMaskMelee)
+
+	if level >= 56 {
+		warlock.AddStat(stats.FirePower, 21)
+		damageMin = 80.0
+		damageMax = 120.0
+		spellId = 17949
+	} else if level >= 46 {
+		warlock.AddStat(stats.FirePower, 17)
+		damageMin = 60.0
+		damageMax = 90.0
+		spellId = 17947
+	} else if level >= 36 {
+		warlock.AddStat(stats.FirePower, 14)
+		damageMin = 40.0
+		damageMax = 60.0
+		spellId = 17945
+	} else if level >= 28 {
+		warlock.AddStat(stats.FirePower, 10)
+		damageMin = 25.0
+		damageMax = 35.0
+		spellId = 758
+	}
+
+	if level >= 28 {
+		fireProcSpell := warlock.GetOrRegisterSpell(core.SpellConfig{
+			ActionID:    core.ActionID{SpellID: spellId},
+			SpellSchool: core.SpellSchoolFire,
+			ProcMask:    core.ProcMaskSpellDamage,
+
+			CritMultiplier:           1.5,
+			DamageMultiplier:         1,
+			ThreatMultiplier:         1,
+			DamageMultiplierAdditive: 1,
+
+			ApplyEffects: func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
+				baseDamage := sim.Roll(damageMin, damageMax) + spellCoeff*spell.SpellPower()
+
+				// TODO: Test if LoF Buffs this
+				//if warlock.LakeOfFireAuras != nil && warlock.LakeOfFireAuras.Get(target).IsActive() {
+				//	baseDamage *= 1.4
+				//}
+
+				spell.CalcAndDealDamage(sim, target, baseDamage, spell.OutcomeMagicCrit)
+			},
+		})
+
+		core.MakePermanent(warlock.GetOrRegisterAura(core.Aura{
+			ActionID: core.ActionID{SpellID: spellId},
+			Label:    "Firestone Proc",
+			OnSpellHitDealt: func(aura *core.Aura, sim *core.Simulation, spell *core.Spell, result *core.SpellResult) {
+				if !result.Landed() {
+					return
+				}
+
+				if !ppm.Proc(sim, spell.ProcMask, "Firestone Proc") {
+					return
+				}
+
+				fireProcSpell.Cast(sim, result.Target)
+			},
+		}))
 	}
 }
 
