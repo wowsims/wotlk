2053327fa768fdafbca7ca3ed30189d9419d4e10
omccabe <omccabe@gmail.com>
2024-01-07 12:18:36 -0500

    add BFD feral set

--- sim/druid/items.go
+++ sim/druid/items.go
@@ -1,4 +1,25 @@
 package druid
 
+import (
+	"github.com/wowsims/sod/sim/core"
+	"github.com/wowsims/sod/sim/core/stats"
+)
+
+
+var ItemSetBlackfathomSlayerLeather = core.NewItemSet(core.ItemSet{
+	Name: "Blackfathom Slayer's Leather",
+	Bonuses: map[int32]core.ApplyEffect{
+		2: func(agent core.Agent) {
+			druid := agent.(DruidAgent).GetDruid()
+			druid.AddStat(stats.AttackPower, 12)
+		},
+		3: func(agent core.Agent) {
+			druid := agent.(DruidAgent).GetDruid()
+			druid.AddStat(stats.MeleeHit, 1)
+			druid.AddStat(stats.SpellHit, 1)
+		},
+	},
+})
+
 func init() {
 }
